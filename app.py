47fbff87b18870f0c2a9071a9f216f420e72b5f3
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2023-07-19 13:17:41 +0530

    component size adjustment

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ if __name__ == "__main__":
     with gr.Blocks(title="Medical Image Segmentation") as demo:
         gr.Markdown("""<h1><center>Medical Image Segmentation with UW-Madison GI Tract Dataset</center></h1>""")
         with gr.Row():
-            img_input = gr.Image(type="pil", height=300, width=300, label="Input image")
-            img_output = gr.AnnotatedImage(label="Predictions", height=300, width=300, color_map=class2hexcolor)
+            img_input = gr.Image(type="pil", height=360, width=360, label="Input image")
+            img_output = gr.AnnotatedImage(label="Predictions", height=360, width=360, color_map=class2hexcolor)
 
         section_btn = gr.Button("Generate Predictions")
         section_btn.click(partial(predict, model=model, preprocess_fn=preprocess, device=DEVICE), img_input, img_output)
